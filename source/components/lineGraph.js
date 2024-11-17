5e4a41358f543f3df78d3f991f6a961cc27c2522
Warren Brown <warrengbrn@gmail.com>
2024-11-17 15:26:30 -0500

    updated ui and chart responsive scaling

--- source/components/lineGraph.js
+++ source/components/lineGraph.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Highcharts, { dateFormat } from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import "../css/main.css";
+import { Dropdown, Container, Row, Col, Form } from 'react-bootstrap';
+
 
 //values must be in the format of yyyy, mm, dd, hh, mm
 const values = 
@@ -10,6 +12,14 @@ const values =
 ;
 
 const options = {
+    chart: {
+        style: {
+            fontFamily: '"Figtree"'
+        },
+        borderRadius: 20,
+        height: (3 / 4 * 100) + '%'
+    },
+
     title: {
         text: 'Economic Data Visualization'
     },
@@ -83,19 +93,60 @@ const options = {
     },
     tooltip: {
         formatter: function () {
-            return `<div style="font-size: 15px;font-weight: bold">${this.series.name}</div>` + '<br> <br> <b>Year: </b>' + dateFormat('%Y', this.key) + `<br> <b>Value:</b> ${this.y}`
+            return `<div style="font-size: 15px;font-weight: bold">
+            ${this.series.name}</div>` + '<br> <br> <b>Year: </b>' + 
+            dateFormat('%Y', this.key) + `<br> <b>Value:</b> ${this.y}`
             
         }
     }
 }
 
 const lineGraph = () => {
+
+    const [filter, setFilter] = useState("GDP (USD Billion)");
+
+
     return(
         <div>
-            <HighchartsReact
-                highcharts={Highcharts}
-                options={options}
-            />
+            <div className='test'>
+                <svg viewBox="25 8 90 150" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <filter id="blurEffect" x="0" y="0" width="200%" height="200%">
+                            <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+                        </filter>
+                    </defs>
+                    <g filter="url(#blurEffect)">
+                        <circle cx="10" cy="50" r="30" fill="#fad196" />
+                        <circle cx="120" cy="70" r="30" fill="#f5bcb7" />
+                    </g>
+                </svg>
+            </div> 
+                    <div className="chartFlex">
+                        <div className="chartContainer">
+                            <div className="boxShadow">
+                                <HighchartsReact  
+                                    highcharts={Highcharts}
+                                    options={options}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+            <Container>
+                <Row>
+                    <h3 className='title'>Filter</h3>
+                </Row>
+                <Row>
+                    <Form.Select className="" aria-label="Default select 
+                    example">
+                        <option value="1">GDP (USD Billion)</option>
+                        <option value="2">Inflation Rate (%)</option>
+                        <option value="3">Unemployment Rate (%)</option>
+                    </Form.Select>
+                </Row>
+                
+            </Container>
+
         </div>
     )
 }
