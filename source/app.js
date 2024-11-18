8ef2a3eedfe316e15e3e247dec04d6cf4daeba21
Warren Brown <warrengbrn@gmail.com>
2024-11-17 19:15:22 -0500

    added checkboxes for series legends, edited css

--- source/app.js
+++ source/app.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../source/css/main.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import LineGraph from "../source/components/lineGraph.js"
 export default function App() {
   const [data, setData] = useState();
--- source/components/lineGraph.js
+++ source/components/lineGraph.js
@@ -1,122 +1,191 @@
 import React, { useState, useEffect, useRef } from 'react'
-import Highcharts, { dateFormat } from 'highcharts'
+import Highcharts, { chart, dateFormat } from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import "../css/main.css";
 import { Dropdown, Container, Row, Col, Form } from 'react-bootstrap';
 
-
-//values must be in the format of yyyy, mm, dd, hh, mm
-const values = 
-    [["2016-11-15T00:00:00.000Z", 2],
-        ["2016-11-16T00:00:00.000Z", 4]]
-;
-
-const options = {
-    chart: {
-        style: {
-            fontFamily: '"Figtree"'
-        },
-        borderRadius: 20,
-        height: (3 / 4 * 100) + '%'
-    },
-
-    title: {
-        text: 'Economic Data Visualization'
-    },
-    subtitle: {
-        text: 'Source: Sameple Dataset'
-    },
-    series: [
-                {
-                name: "USA",
-                data: [
-                    [Date.parse('2010-12-31T23:59:00Z'), 15000], 
-                    [Date.parse('2011-12-31T23:59:00Z'), 15500],
-                    [Date.parse('2012-12-31T23:59:00Z'), 16000]
-                    ],
-                },
-                {
-                name: "Canada",
-                data: [
-                    [Date.parse('2010-12-31T23:59:00Z'), 1400], 
-                    [Date.parse('2011-12-31T23:59:00Z'), 1450],
-                    [Date.parse('2012-12-31T23:59:00Z'), 1500]
-                    ],
-                },
-                {
-                name: "Germany",
-                data: [
-                    [Date.parse('2010-12-31T23:59:00Z'), 3300], 
-                    [Date.parse('2011-12-31T23:59:00Z'), 3400],
-                    [Date.parse('2012-12-31T23:59:00Z'), 3500]
-                    ],
-                },
-                {
-                name: "India",
-                data: [
-                    [Date.parse('2010-12-31T23:59:00Z'), 1700], 
-                    [Date.parse('2011-12-31T23:59:00Z'), 1800],
-                    [Date.parse('2012-12-31T23:59:00Z'), 1900]
-                    ],
-                },
-                {
-                name: "Brazil",
-                data: [
-                [Date.parse('2010-12-31T23:59:00Z'), 2200], 
-                [Date.parse('2011-12-31T23:59:00Z'), 2300],
-                [Date.parse('2012-12-31T23:59:00Z'), 2400]
-                    ],
-                },
-            ],
-    credits: {
-        enabled: false
-    },
-    xAxis: {
-        crosshair: {
-            snap: true
-        },
-        gridLineWidth: 1,
-        type: 'datetime',
-        labels: {
-            format: '{value:%Y}',
-        }
-    },
-    yAxis: {
-        crosshair: {
-            snap: true
-        },
-    },
-    plotOptions: {
-        series: {
-            allowPointSelect: true
-        }
-    },
-    tooltip: {
-        formatter: function () {
-            return `<div style="font-size: 15px;font-weight: bold">
-            ${this.series.name}</div>` + '<br> <br> <b>Year: </b>' + 
-            dateFormat('%Y', this.key) + `<br> <b>Value:</b> ${this.y}`
-            
-        }
-    }
-}
-
 const lineGraph = () => {
+
+    const chartRef = useRef(null);
 
     const [filter, setFilter] = useState("GDP (USD Billion)");
 
+    const handleFilterChange = (event) => {
+        console.log("TEST")
+        setFilter(event);
+    }
+
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+
+      useEffect(() => {
+        function handleResize() {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+
+        window.addEventListener('resize', handleResize);
+    
+        return () => window.removeEventListener('resize', handleResize);
+      }, []);
+
+      const options = {
+        chart: {
+            style: {
+                fontFamily: '"Figtree"'
+            },
+            borderRadius: 20,
+            height: (windowSize.height * .65),
+            marginRight: 50,
+            borderColor: '#334eff',
+        },
+    
+        responsive: {
+            rules: [{
+                condition: {
+                }
+            }]
+        },
+        title: {
+            text: filter,
+            style: {
+                color: "#636363"
+            }
+        },
+        subtitle: {
+            text: 'Source: Sample Dataset'
+        },
+        series: [
+                    {
+                    name: "USA",
+                    data: [
+                        [Date.parse('2010-12-31T23:59:00Z'), 15000], 
+                        [Date.parse('2011-12-31T23:59:00Z'), 15500],
+                        [Date.parse('2012-12-31T23:59:00Z'), 16000]
+                        ],
+                    selected: true
+                    },
+                    {
+                    name: "Canada",
+                    data: [
+                        [Date.parse('2010-12-31T23:59:00Z'), 1400], 
+                        [Date.parse('2011-12-31T23:59:00Z'), 1450],
+                        [Date.parse('2012-12-31T23:59:00Z'), 1500]
+                        ],
+                    selected: true
+                    },
+                    {
+                    name: "Germany",
+                    data: [
+                        [Date.parse('2010-12-31T23:59:00Z'), 3300], 
+                        [Date.parse('2011-12-31T23:59:00Z'), 3400],
+                        [Date.parse('2012-12-31T23:59:00Z'), 3500]
+                        ],
+                    selected: true
+                    },
+                    {
+                    name: "India",
+                    data: [
+                        [Date.parse('2010-12-31T23:59:00Z'), 1700], 
+                        [Date.parse('2011-12-31T23:59:00Z'), 1800],
+                        [Date.parse('2012-12-31T23:59:00Z'), 1900]
+                        ],
+                    selected: true
+                    },
+                    {
+                    name: "Brazil",
+                    data: [
+                    [Date.parse('2010-12-31T23:59:00Z'), 2200], 
+                    [Date.parse('2011-12-31T23:59:00Z'), 2300],
+                    [Date.parse('2012-12-31T23:59:00Z'), 2400]
+                        ],
+                    selected: true
+                    },
+                ],
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            crosshair: {
+                snap: true
+            },
+            gridLineWidth: 1,
+            type: 'datetime',
+            labels: {
+                format: '{value:%Y}',
+            }
+        },
+        yAxis: {
+            crosshair: {
+                snap: true
+            },
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true,
+                showCheckbox: true,
+                events: {
+                    checkboxClick: function (event) {
+                        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+                        checkboxes.forEach(
+                            (checkbox, index) => {
+                                checkbox.id = `checkbox-${index + 1}`;
+                            }
+                        )
+                        checkboxes.forEach((checkbox, index) => {
+                            if (checkbox.checked){
+                                console.log(`Checkbox with ID ${index} is clicked`);
+                            } else {
+                                console.log(`Checkbox with ID ${index} is NOT clicked`);
+                            }
+                        })
+                        if (chartRef.current) {
+                            const chart = chartRef.current.chart;
+                            // chart.series[1].hide();
+                            checkboxes.forEach((checkbox, index) => {
+                                if (!checkbox.checked){
+                                    chart.series[index].hide()
+                                } else {
+                                    chart.series[index].show()
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            events: {
+                itemClick: function () {
+                    return false;
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                return `<div style="font-size: 15px;font-weight: bold">
+                ${this.series.name}</div>` + '<br> <br> <b>Year: </b>' + 
+                dateFormat('%Y', this.key) + `<br> <b>Value:</b> ${this.y}`
+                
+            }
+        }
+    }
 
     return(
         <div>
             <div className='test'>
-                <svg viewBox="25 8 90 150" xmlns="http://www.w3.org/2000/svg">
+                <svg viewBox="25 8 90 200" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <filter id="blurEffect" x="0" y="0" width="200%" height="200%">
                             <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
                         </filter>
                     </defs>
                     <g filter="url(#blurEffect)">
-                        <circle cx="10" cy="50" r="30" fill="#fad196" />
+                        <circle cx="10" cy="35" r="30" fill="#fad196" />
                         <circle cx="120" cy="70" r="30" fill="#f5bcb7" />
                     </g>
                 </svg>
@@ -125,24 +194,34 @@ const lineGraph = () => {
                         <div className="chartContainer">
                             <div className="boxShadow">
                                 <HighchartsReact  
+                                    className="restrict"
                                     highcharts={Highcharts}
                                     options={options}
+                                    ref={chartRef}
                                 />
                             </div>
                         </div>
                     </div>
 
-            <Container>
+            <Container className=''>
                 <Row>
-                    <h3 className='title'>Filter</h3>
+                    <h1 className='chartTitle'>Economic Data Visualization</h1>
+                </Row>
+                <Row className='mx-5'>
+                    <div className='chartDescription pt-4'>Analysis of a dataset containing 
+                        economic indicators 
+                        across multiple countries over a series of years.</div>
                 </Row>
                 <Row>
-                    <Form.Select className="" aria-label="Default select 
-                    example">
-                        <option value="1">GDP (USD Billion)</option>
-                        <option value="2">Inflation Rate (%)</option>
-                        <option value="3">Unemployment Rate (%)</option>
-                    </Form.Select>
+                    <h3 className='title pt-5'>Filter</h3>
+                </Row>
+                <Row>
+                    <Form.Control className="w-25" as="select" onChange={(e) => 
+                        handleFilterChange(e.target.value)}>
+                        <option value="GDP (USD Billion)">GDP (USD Billion)</option>
+                        <option value="Inflation Rate (%)">Inflation Rate (%)</option>
+                        <option value="Unemployment Rate (%)">Unemployment Rate (%)</option>
+                    </Form.Control>
                 </Row>
                 
             </Container>
