28fc457c180ce95769a6efe705add51ef79efb48
Warren Brown <warrengbrn@gmail.com>
2024-11-19 04:46:30 -0500

    organized imports & removed unused imports

--- source/index.js
+++ source/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './app';
+import "../source/css/main.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // provides a root to display react components inside the browser DOM
 const root = createRoot(document.getElementById('App'));
 
 // renders the root component 
-root.render(<BrowserRouter><App/></BrowserRouter>)
+root.render(<App/>)
