95d6f9dfe16b3ea76c7454ff0fc23c95ab7699cd
Warren Brown <warrengbrn@gmail.com>
2024-11-19 04:44:13 -0500

    organized imports

--- source/app.js
+++ source/app.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "../source/css/main.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import LineGraph from "../source/components/lineGraph.js"
 
 export default function App() {
